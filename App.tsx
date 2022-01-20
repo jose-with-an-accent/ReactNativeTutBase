7c197506be3c099ae05f6ed5a64c4c1437d7e9ea
Jose Sanchez <hi@josesanchez.dev>
2022-01-20 00:00:25 -0600

    made a few comments to simplify things

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+// used for navigation
+const Stack = createNativeStackNavigator()
 
 export default function App() {
+  //pages can be added inside the Stack.Navigator w/ a similar format
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+function HomeScreen({ navigation }) {
+  //to move between pages, use navigation.navigate(name)
+  useEffect(() => {
+    async function getData() {
+      // this function is inside to avoid issues w/ react callbacks experienced earlier
+      // put logic to get data from network here
+    }
+    getData()
+  }, [])
+  return (
+    <View>
+      <Text>Top Anime</Text>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# React Native Project (w/ React-Navigation)
+
+This is just a simple project base for (future) tutorials.
